6242d5f01f10b4ad59121cce23afe647750fbc6d
Paul Walker <wpaulsg@gmail.com>
2020-06-28 13:00:37 -0500

    python: added argparse example

--- python/main.py
+++ python/main.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
+import argparse
+
 
 def main():
-    pass
+    args = parse_args()
+    print("Operating on file '{}'".format(args.file.name))
+    if args.verbose:
+        print("Verbose mode enabled")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'),
+                        help="file to process")
+    parser.add_argument("--verbose", action="store_true",
+                        help="increase output verbosity")
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
